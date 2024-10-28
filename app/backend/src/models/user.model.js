2aa6e49603b1052c53260c544a6f28276cb51679
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2024-10-28 14:11:43 -0600

    fix: user model

--- app/backend/src/models/user.model.js
+++ app/backend/src/models/user.model.js
@@ -4,8 +4,8 @@ import { sequelize } from "../config/db.config.js";
 const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    name: { type: DataTypes.STRING, allowNull: false, unique: true },
-    lastname: { type: DataTypes.STRING, allowNull: false, unique: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    lastname: { type: DataTypes.STRING, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
     balance: { type: DataTypes.DECIMAL(10, 2), defaultValue: 0.00 },
     status: {
